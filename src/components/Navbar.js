7ade0e4ce189bbebb2b895ef4eab2a280ac42c66
Debuger <hello@debuger.codes>
2021-08-05 08:39:10 +0000

    Logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Logo, NavbarContainer } from '../styles/NavbarStyle'
+
+function Navbar() {
+    return (
+        <NavbarContainer>
+          <Logo>
+            Debugger
+          </Logo>
+        </NavbarContainer>
+    )
+}
+
+export default Navbar
---
+++ src/styles/NavbarStyle.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Logo = styled.div`
+            min-height: 72px;
+            font-family: Poppins;
+            font-weight: 700;
+            color: rgba(0, 0, 0, 1);
+            font-size: 30px;
+            letter-spacing: 0;
+            padding-left: 10px;
+            padding-top: 5px;
+`
+
+export const NavbarContainer = styled.div`
+   align-items: center;
+   justify-content: center;
+`
